fe24177a7ca319bb215a0cb1fa7cb7de1d033bd3
m4cren <renrenofficial@gmail.com>
2025-06-03 21:36:41 +0800

    implemented cloudinary

---
+++ app/upload/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
+import { useEffect, useState } from "react";
+
+type UploadResultTypes = {
+   public_id: string;
+};
+
+const UploadPage = () => {
+   const [publicId, setPublicId] = useState("");
+   useEffect(() => {
+      if (!publicId) return;
+      console.log(publicId);
+   }, [publicId]);
+   return (
+      <>
+         {publicId && (
+            <CldImage src={publicId} width={270} height={180} alt="A review" />
+         )}
+         <CldUploadWidget
+            options={{
+               sources: ["local"],
+            }}
+            onSuccess={(result) => {
+               const res = result.info as UploadResultTypes;
+               if (result.event === "success") {
+                  setPublicId(res.public_id);
+               }
+            }}
+            uploadPreset="m4cren"
+         >
+            {({ open }) => (
+               <button className="btn btn-primary" onClick={() => open()}>
+                  Upload
+               </button>
+            )}
+         </CldUploadWidget>
+      </>
+   );
+};
+
+export default UploadPage;
